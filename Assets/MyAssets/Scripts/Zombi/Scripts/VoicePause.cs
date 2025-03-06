1c7abad2ac99a353fd61f944b08e8bfc43f9d4fe
alexey231090 <alexey231090@gmail.com>
2025-03-06 14:12:21 +0700

    V-1.34
    Триггеры и ограничители уровня расставлены на 1 уровне, Добавлен голос у зомби(Добавить туман, и сделать прорисовку только на уровне тумана)

---
+++ Assets/MyAssets/Scripts/Zombi/Scripts/VoicePause.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections;
+
+
+namespace FpsZomby { 
+  public class VoicePause : MonoBehaviour
+    {
+        Zombi zombie;
+        public AudioSource voiceSound; // Ссылка на AudioSource
+        public float pauseDuration = 3f; // Длительность паузы в секундах
+        private bool isCoroutineRunning = false;
+
+        private void Start()
+        {
+            zombie = gameObject.GetComponent<Zombi>();
+
+            if (voiceSound == null)
+            {
+                Debug.LogError("AudioSource не назначен!");
+                return;
+            }
+        }
+
+        private void Update()
+        {
+            // Запускаем корутину для воспроизведения звука с паузами
+            if (zombie.statusZombi == Zombi.ZombiStatus.run && !isCoroutineRunning)
+            {
+                StartCoroutine(PlaySoundWithPause());
+            }
+        }
+
+        private IEnumerator PlaySoundWithPause()
+        {
+            isCoroutineRunning = true;
+            while(zombie.statusZombi != Zombi.ZombiStatus.dead) // Проверяем статус зомби
+            {
+                // Воспроизводим звук
+                voiceSound.Play();
+
+                // Ждем, пока звук закончится
+                yield return new WaitForSeconds(voiceSound.clip.length);
+
+                // Пауза перед следующим воспроизведением
+                yield return new WaitForSeconds(pauseDuration);
+            }
+            isCoroutineRunning = false;
+        }
+    }
+
+    
+
+}
